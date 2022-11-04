c6f0d412a6059534e97cb59d8d8589c42b78b76a
adilahamad786 <adilahamad.personally@gmail.com>
2022-11-04 12:40:39 +0530

    Adding button inside the Greeting component to manipulate content and also testing button clicked or not renders.

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Greeting = () => {
+  const [change, setChange] = useState(false);
+
+  const changeParagraph = () => {
+    setChange(state => !state.change);
+  }
+
   return (
     <>
       <div>Hello World!</div>
-      <p>It's good to see you!</p>
+      { !change && <p>It's good to see you!</p> }
+      { change && <p>Change!</p> }
+      <button onClick={changeParagraph}>Change</button>
     </>
   );
 };
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,14 +1,42 @@
 import Greeting from './Greeting';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 
-test("Render Hello World as text", () => {
-    // Arrange Part/Section
-    render(<Greeting />);
+describe("Greeting component", () => { // create similar testing group.
+    test('Render "Hello World" as text', () => {
+        // Arrange Part/Section
+        render(<Greeting />);
 
-    // Act Part/Section
-    // ...
+        // Act Part/Section
+        // ...
 
-    // Assert
-    const helloWorldElement = screen.getByText('Hello World', { exact : false });
-    expect(helloWorldElement).toBeInTheDocument();
-})
+        // Assert
+        const helloWorldElement = screen.getByText('Hello World', { exact : false });
+        expect(helloWorldElement).toBeInTheDocument();
+    });
+
+    test('renders "good to see you" if button was NOT clicked', () => {
+        // Arrange Part/Section
+        render(<Greeting />);
+
+        // Act Part/Section
+        // ...
+
+        // Assert
+        const outputElement = screen.getByText('good to see you', { exact : false });
+        expect(outputElement).toBeInTheDocument();
+    });
+
+    test('renders "Change!" if button was clicked', () => {
+        // Arrange Part/Section
+        render(<Greeting />);
+
+        // Act Part/Section
+        const buttonElement = screen.getByRole('button');
+        userEvent.click(buttonElement)
+
+        // Assert
+        const outputElement = screen.getByText("Change!");
+        expect(outputElement).toBeInTheDocument()
+    })
+});

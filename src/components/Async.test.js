86902d842a786d6e7935a662c55cb44b0d2a92ff
adilahamad786 <adilahamad.personally@gmail.com>
2022-11-04 16:50:22 +0530

    Best way to test component which are contain async task like request or fetching data from server.

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,6 +3,12 @@ import Async from "./Async";
 
 describe("Async component", () => {
     test('renders posts if request successed', async () => {
+        // own mock function which work like fetch function and resolve overhead of networking request for each testing and also our target is testing app component not fetch function because fetch function is maintain by browser.
+        window.fetch = jest.fn();
+        window.fetch.mockResolvedValueOnce({
+            json : async () => [{ id : 'p1', title : "This is first item" }]
+        })
+        
         render(<Async />);
 
         const listItemElements = await screen.findAllByRole('listitem');

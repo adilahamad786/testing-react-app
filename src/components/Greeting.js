84eba9b2d53db9ca7528dccb52b418dd2bae3139
adilahamad786 <adilahamad.personally@gmail.com>
2022-11-04 11:05:08 +0530

    Adding Greeting component.

---
+++ src/components/Greeting.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Greeting = () => {
+  return (
+    <>
+      <div>Hello World!</div>
+      <p>It's good to see you!</p>
+    </>
+  );
+};
+
+export default Greeting;
